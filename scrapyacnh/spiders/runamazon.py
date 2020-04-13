f6309afcde85a55801e83145730ab29462e88703
Jade Lau <jadeyslau@gmail.com>
2020-04-13 17:30:32 +0100

    Separate run file

--- scrapyacnh/spiders/runamazon.py
+++ scrapyacnh/spiders/runamazon.py
@@ -1,9 +1,9 @@
-# from scrapy.crawler import CrawlerProcess
-# from scrapy.utils.project import get_project_settings
-#
-# process = CrawlerProcess(get_project_settings())
-#
-# process.crawl('amazon_bot', domain='amazon.co.uk')
-# process.crawl('amazon_bot_test', domain='amazon.co.uk')
-#
-# process.start()
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(get_project_settings())
+
+process.crawl('amazon_bot', domain='amazon.co.uk')
+process.crawl('amazon_bot_test', domain='amazon.co.uk')
+
+process.start()
